bb875ece14baeef2f87309234dfc0bf40d24fb5b
The-Drummer6 <fabian@raabonline.eu>
2017-06-05 12:43:19 +0200

    Kopie

--- EnterpriseProductionSolution/EnterpriseProductionSolution/DashoardForm.Designer.cs
+++ EnterpriseProductionSolution/EnterpriseProductionSolution/DashoardForm.Designer.cs
@@ -28,19 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.metroTile1 = new MetroFramework.Controls.MetroTile();
             this.SuspendLayout();
+            // 
+            // metroTile1
+            // 
+            this.metroTile1.ActiveControl = null;
+            this.metroTile1.Location = new System.Drawing.Point(35, 136);
+            this.metroTile1.Name = "metroTile1";
+            this.metroTile1.Size = new System.Drawing.Size(227, 120);
+            this.metroTile1.TabIndex = 0;
+            this.metroTile1.Text = "XML Import";
+            this.metroTile1.UseSelectable = true;
             // 
             // DashoardForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1162, 684);
+            this.Controls.Add(this.metroTile1);
             this.Name = "DashoardForm";
-            this.Text = "DashoardForm";
+            this.Text = "Enterprise Planning Solution";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private MetroFramework.Controls.MetroTile metroTile1;
     }
 }
